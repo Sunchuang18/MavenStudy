8eb0449e71a1c3cb705150cb90ec2877c09addf4
Mr.Sun <88422339+Sunchuang18@users.noreply.github.com>
2023-04-01 20:51:11 +0800

    pull commit

--- src/main/java/com/sun/git/GitTest.java
+++ src/main/java/com/sun/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("master更改!");
         System.out.println("hot-fix更改!");
+        System.out.println("GitHub更改!");
     }
 }
